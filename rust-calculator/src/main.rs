cee968256f31ceb48bbd28ccc05b89d38eb92a50
C4X2 <nathaniel.brown.15@cnu.edu>
2020-01-22 21:23:57 -0500

    Starting to make a calculator

--- rust-calculator/src/main.rs
+++ rust-calculator/src/main.rs
@@ -11,22 +11,21 @@ use std::io;
 * indicate there are no parameters, and the curly bracket, {, starts the body of the function.
 */
 fn main() {
-    println!("Hello, world!");
 
-    println!("Guess the number!");
-
-    println!("Please enter your guess!");
+    println!("Please enter your first number!");
     /*
-    *   The 'let' keyword is used for variable declarationwhile
+    *  The 'let' keyword is used for variable declaration, while
     *  the 'mut' keyword denotes a variable that is mutable since recall that all variables are
     *  immutable by default in rust.
-    */
-    let mut guess = String::new();
-    /* The :: syntax in the ::new line indicates that new is an associated function of the String type.
+    *
+    *  The :: syntax in the ::new line indicates that new is an associated function of the String type.
     *  An associated function is implemented on a type,
     *  in this case String, rather than on a particular instance of a String.
     *  Some languages call this a static method.
+    *
     */
+    let mut number1 = String::new();
+
 
 
     /*
@@ -48,7 +47,7 @@ fn main() {
     * take the return value that Ok is holding and return just that value to you so you can use it.
     * In this case, that value is the number of bytes in what the user entered into standard input.
     */
-    io::stdin().read_line(&mut guess) // Returns a result type
+    io::stdin().read_line(&mut number1) // Returns a result type
         .expect("Failed to read given input!"); // lines seperated for clarity
 
     /* This line prints the string we saved the user’s input in. The set of curly brackets, {},
@@ -56,5 +55,120 @@ fn main() {
     *  You can print more than one value using curly brackets: the first set of curly brackets
     *  holds the first value listed after the format string, the second set holds the second value, and so on
     */
-    println!("You guessed: {}", guess);
+    //println!("You guessed: {}", guess);
+
+    println!("Please enter your second number!");
+
+    let mut number2 = String::new();
+
+    io::stdin().read_line(&mut number2) // Returns a result type
+        .expect("Failed to read given input!"); // lines seperated for clarity
+
+    let mut operation = String::new();
+
+    /*
+    * In Rust the type of an array does not have to be explicitly stated if since they have to share the same type.
+    * If one were to explicitly state the array declarion it would be [&str; 5] where [TYPE; LENGTH].
+    */
+    let arr = ["Addition", "Subtraction", "Multiplication", "Division", "Modulo"];
+
+    /*
+    * Again, Rust allows us to declare the type of the varible before use or initialization.
+    * In this specifc case, it is very redunant becuase the String::with_capacity method creates
+    * and returns a new string, so the type of words could be inferred but never the less it is
+    * good to know.
+    */
+    let mut words : String = String::with_capacity(40);
+    /*
+    * A basic for loop in rust. It takes an iterator type prodcued by the array and
+    * bonds that value to the scoped variable i.
+    *
+    */
+    for i in arr.iter()
+    {
+        /*
+        * push_str in Rust is very similar to the .append method in Java (from the StringBuffer class)
+        */
+        words.push_str(i);
+        words.push_str(" ");
+    }
+    println!("Please enter your intedend operation! Your choices are {}", words);
+
+
+    io::stdin().read_line(&mut operation) // Returns a result type
+        .expect("Failed to read given input!"); // lines seperated for clarity
+    /*
+    while !verify_input(&operation){
+        println!("Please enter your intedend operation! Your choices are {}", words);
+
+
+        io::stdin().read_line(&mut operation) // Returns a result type
+            .expect("Failed to read given input!"); // lines seperated for clarity
+    }
+    */
+    let mut result = String::new();
+
+    for i in arr.iter()
+    {
+        if operation.trim().eq_ignore_ascii_case(i)
+        {
+            /*
+            * This assignment works becuase the String struct in rust can grow dynamically,
+            * so we don't have to worry about overflow of pre-allocated space.
+            */
+            result = i.to_string();
+        }
+    };
+
+    /*
+    * Gotta be honest didn't think functional composition would work in this way but it does,
+    * the thing is if this were a 'real' program we would want to transform the String struct to a
+    * i64 number immediately in order to save space.
+    */
+    let final_result = calculator(operation, transform_input(number1.as_mut_str()), transform_input(number2.as_mut_str()));
+
+    // Print the result
+    println!("Your result is {}", final_result);
+}
+/*
+* We want to verify that the given user operation is actually a viable one.
+*
+*
+*/
+fn verify_input(input : &str) -> bool {
+    // TODO make this variable flushable 
+    let mut match_pattern = input.trim();
+    println!("MatchPattern : {}", match_pattern);
+    return match match_pattern
+    {
+        "Addition"| "Subtraction"| "Multiplication"| "Division"| "Modulo" => true,
+        _ => false
+    };
+}
+/*
+*
+*
+* @parameters
+*/
+fn transform_input (input : &str) -> i64 {
+    return match input.trim().parse::<i64>() {
+        Ok(n) => n,
+        Err(e) => 0
+    };
+}
+
+fn calculator(operation : String, number1 : i64, number2: i64) -> i64 {
+    let result = if "Addition".eq_ignore_ascii_case(operation.trim()) {
+        return number1 + number2;
+    } else if "Multiplication".eq_ignore_ascii_case(operation.trim()) {
+        return number1 * number2;
+    } else if "Subtraction".eq_ignore_ascii_case(operation.trim()) {
+        return number1 - number2;
+    } else if "Division".eq_ignore_ascii_case(operation.trim()) {
+        return number1 / number2;
+    } else if "Modulo".eq_ignore_ascii_case(operation.trim()) {
+        return number1 % number2;
+    } else {
+        return 0;
+    };
 }
